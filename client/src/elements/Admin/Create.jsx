327b945a2d7cd68a57d91d1b36be243fb26575ac
ElmerG <gonzagaelmer0597@gmail.com>
2025-05-30 11:24:17 +0800

    save mod

--- client/src/elements/Admin/Create.jsx
+++ client/src/elements/Admin/Create.jsx
@@ -13,6 +13,14 @@ function Create() {
         cre_password: '',
         cre_confirm_password: ''
     })
+    const [selectedImage, setSelectedImage] = useState(null)
+    const [uploadStatus, setUploadStatus] = useState('')
+    const formData = new FormData()
+
+    const handleFileChange = (e) => {
+        const imageFile = e.target.files[0]
+        setSelectedImage(imageFile ? URL.createObjectURL(imageFile) : undefined)
+    }
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -20,34 +28,53 @@ function Create() {
         if (values.cre_name === "" || values.cre_email === "" || values.cre_age === "") {
             alert('Please fill out the form properly.')
         } else {
-            // send post request
-            axios.post('/add_student', values)
-            .then((res) => {
-                console.log(JSON.stringify(res))
-                if (res.data.error !== undefined) {
-                    console.log("if")
-                    alert(res.data.error)
-                } else {
-                    console.log("else")
-                    if (closeButtonRef.current) {
-                        closeButtonRef.current.click();
-                    }
-                    alert(res.data.success)
-                }
-                console.log("out")
-            })
-            .catch((err) => {
-                alert(AXIOS_ERR_MSG)
-                console.log(err)
-            })
+            if (!selectedImage) {
+                setUploadStatus('Please select a file/photo.')
+                return
+            }
+
+            formData.append('name', values.cre_name)
+            formData.append('email', values.cre_email)
+            formData.append('gender', values.cre_gender)
+            formData.append('age', values.cre_age)
+            formData.append('image', selectedImage)
+
+            try {
+                // requestSave(values)
+            } catch(e) {
+
+            }
         }
     } 
+
+    function requestSave(values) {
+        // send post request
+        axios.post('/add_student', formData)
+        .then((res) => {
+            console.log(JSON.stringify(res))
+            if (res.data.error !== undefined) {
+                console.log("if")
+                alert(res.data.error)
+            } else {
+                console.log("else")
+                if (closeButtonRef.current) {
+                    closeButtonRef.current.click();
+                }
+                alert(res.data.success)
+            }
+            console.log("out")
+        })
+        .catch((err) => {
+            alert(AXIOS_ERR_MSG)
+            console.log(err)
+        })
+    }
 
   return (
     <div className='row justify-content-center my-4 mx-2'>
         <h3>Adding a Student</h3>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} encType='multipart/form-data' >
             <div className='row my-3 px-1'>
                 <div className='col-6'>
                     <label htmlFor='name'>Name</label>
@@ -83,8 +110,26 @@ function Create() {
                     />
                 </div>
             </div>
+
+            <div className='row my-3 px-1'>
+                <div className='col-12'>
+                    <input type="file" accept="image/*" name="file" onChange={handleFileChange} required/>
+                    {
+                        selectedImage && (
+                            <img 
+                                src={selectedImage}
+                                width="auto"
+                                height={200}
+                                alt="Selected Image"
+                                className="img img-thumbnail mt-2"/>
+                        )
+                    }
+                </div>
+            </div>
+
+            {/* note */}
+            <p className='text-muted text-center px-1 fst-italic fw-light'>IMPORTANT: The student's default password is: { DEFULT_STUDENT_PASS }</p>
             
-            <p className='text-danger text-center px-1'>NOTE: The student's default password is: { DEFULT_STUDENT_PASS }</p>
             {/* buttons */}
             <div className="d-grid gap-2 d-md-flex justify-content-md-end mt-4">
                 <button type='submit' className='btn btn-success mx-1'>Add Student</button>
